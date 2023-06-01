97dd5d953a0b993eec835b2f1fd8e4fd0e617f06
KishanPanchal25 <k.d.panchalofc@gmail.com>
2023-06-01 16:44:32 +0530

    Cancle Expenses Entry

--- expense_pay/create_gl_entry.py
+++ expense_pay/create_gl_entry.py
@@ -53,3 +53,62 @@ def create_gl_entries(doc, method):
         gle.flags.ignore_permissions = 1
         gle.flags.notify_update = False
         gle.submit()
+
+
+def cancel_gl_entries(doc, method):
+
+    # Ignore linked doctypes
+    doc.ignore_linked_doctypes = ("GL Entry",)    
+    
+    
+    gl_entries = []
+    
+    # Create GL entry for Account Paid From
+    gl_entry = {
+        "doctype": "GL Entry",
+        "posting_date": doc.posting_date,
+        "account": doc.account_paid_from,
+        "cost_center": doc.default_cost_center,
+        "debit": doc.paid_amount,
+        "credit": 0,
+        "debit_in_account_currency": doc.paid_amount,
+        "credit_in_account_currency": 0,
+        "against": ", ".join([d.account_paid_to for d in doc.expenses]),
+        "voucher_type": _("Expenses Entry"),
+        "voucher_no": doc.name,
+        "is_opening": "No",
+        "is_advance": "No",
+        "fiscal_year": frappe.defaults.get_user_default("fiscal_year"),
+        "company": doc.company,
+        "is_cancelled": 1
+    }
+    gl_entries.append(gl_entry)
+
+    # Create GL entries for Expenses child table
+    for expense in doc.expenses:
+        gl_entry = {
+            "doctype": "GL Entry",
+            "posting_date": doc.posting_date,
+            "account": expense.account_paid_to,
+            "cost_center": expense.cost_center,
+            "debit": 0,
+            "credit": expense.amount,
+            "debit_in_account_currency": 0,
+            "credit_in_account_currency": expense.amount,
+            "against": doc.account_paid_from,
+            "voucher_type": _("Expenses Entry"),
+            "voucher_no": doc.name,
+            "is_opening": "No",
+            "is_advance": "No",
+            "fiscal_year": frappe.defaults.get_user_default("fiscal_year"),
+            "company": doc.company,
+            "is_cancelled": 1
+        }
+        gl_entries.append(gl_entry)
+
+    for gl_entry in gl_entries:
+        gle = frappe.new_doc("GL Entry")
+        gle.update(gl_entry)
+        gle.flags.ignore_permissions = 1
+        gle.flags.notify_update = False
+        gle.submit()
