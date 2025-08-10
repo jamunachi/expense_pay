1e5af3e92dd1e9c50d312e754063072b4d9ea1d1
jamunachi <83000184+jamunachi@users.noreply.github.com>
2025-08-11 01:24:05 +0300

    Update expense_entry_settings.py

--- expense_pay/expense_pay/doctype/expense_entry_settings/expense_entry_settings.py
+++ expense_pay/expense_pay/doctype/expense_entry_settings/expense_entry_settings.py
@@ -1,36 +1,33 @@
 # Copyright (c) 2025, Kishan Panchal and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ExpenseEntrySettings(Document):
-	pass
-<<<<<<< HEAD
-=======
+    pass
 
-
-import frappe
 
 @frappe.whitelist()
 def get_ui_filters():
     s = frappe.get_single("Expense Entry Settings")
+
     def to_list(val):
-        if not val: return []
+        if not val:
+            return []
         out = []
         for line in val.splitlines():
-            for p in line.split(','):
+            for p in line.split(","):
                 p = p.strip()
-                if p: out.append(p)
+                if p:
+                    out.append(p)
         return out
-    return {
-        "only_leaf_accounts": int(s.only_leaf_accounts or 0),
-        "only_leaf_cost_centers": int(s.only_leaf_cost_centers or 0),
-        "debit_account_types": to_list(s.debit_account_types),
-        "credit_account_types": to_list(s.credit_account_types),
-    ,
-        "enable_import_ui": int(s.enable_import_ui or 0)
-    }
 
->>>>>>> origin/release/v1.0.0
+    return {
+        "only_leaf_accounts": int(getattr(s, "only_leaf_accounts", 0) or 0),
+        "only_leaf_cost_centers": int(getattr(s, "only_leaf_cost_centers", 0) or 0),
+        "debit_account_types": to_list(getattr(s, "debit_account_types", "")),
+        "credit_account_types": to_list(getattr(s, "credit_account_types", "")),
+        "enable_import_ui": int(getattr(s, "enable_import_ui", 0) or 0),
+    }
